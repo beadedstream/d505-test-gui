ce618ec7dec5fa6f337311e1d59c2b33be0a97f5
Electrickal <erickjm.eng@gmail.com>
2019-12-20 16:19:39 -0800

    upversion to 1.1.4

--- app/views.py
+++ app/views.py
@@ -14,7 +14,7 @@ from PyQt5.QtGui import QPixmap, QFont
 from PyQt5.QtCore import QSettings, Qt, QThread
 
 
-VERSION_NUM = "1.1.3"
+VERSION_NUM = "1.1.4"
 
 WINDOW_WIDTH = 1550
 WINDOW_HEIGHT = 900
